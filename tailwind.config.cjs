4c07a9e80b1eddbed2e8a145fb843b9710126dd3
Marek Rewers <hello@marek.dev>
2021-05-23 11:41:51 -0400

    feat: customize colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,30 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.js'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      canvas: {
+        light: '#F5F1ED',
+        DEFAULT: '#DAD2BC',
+        dark: '#A99985'
+      },
+      raisin: {
+        DEFAULT: '#252323'
+      },
+      slate: {
+        DEFAULT: '#70798C'
+      },
+      black: colors.black,
+      gray: colors.trueGray,
+      green: colors.emerald,
+      indigo: colors.indigo,
+      red: colors.rose,
+      white: colors.white,
+      yellow: colors.amber,
+    }
+  }
 }
